ff9cc55f8be64cc4aeaf801f14ca96bbf295be50
soushi <soushi_mizuno@outlook.jp>
2021-09-30 16:45:34 +0900

    デザインの変更

--- js/view.js
+++ js/view.js
@@ -1,64 +1,3 @@
-function htmlInit(title,value){
-    $("body").append(`
-    <div id="ex-score">
-        <header id="ex-header" class="ex-header"><div id="ex-header-icon"></div><div id="back-button"></div><p>${title}</p></header>
-        <div id="ex-body">
-            <div class="pie">${value}%</div>
-            <ul id="content-list" class="item-list">
-            </ul>
-        </div>
-    </div>
-    `);
-    const iconPass =  chrome.extension.getURL("img/data_usage.svg");
-    $("#ex-score #back-button").css("background-image",`url(${iconPass})`);
-    $("#ex-score .pie").css("--pie-value", `${value}%`);
-}
-
-function setTitle(title){   
-    $("#ex-score header p").text(title);
-}
-
-function setPieValue(value){
-    $("#ex-score .pie").text(`${value}%`);
-    $("#ex-score .pie").css("--pie-value", `${value}%`);
-}
-
-let id = 0;
-
-function addListContents(title,value,callback){
-    $("#ex-score #content-list").append(`
-    <li id="ex-score-${id}">
-        <div class="bar"><div class="bar-val" style="width:${value}%;"></div></div>
-        <div class="list-content">
-            <div class="list-title">${title}</div>
-            <div class="list-value">進捗度${value}%</div>
-        </div>
-    </li>
-    `);
-    $(`#ex-score-${id}`).on("click", callback);
-    id ++;
-}
-
-function refreshList(){
-    for (let i = 0; i < id; i++) {
-        $(`#ex-score-${i}`).off("click");
-    }
-    $("#ex-score #content-list").empty();
-    id = 0;
-}
-
-function setBackButton(callback){
-    const iconPass =  chrome.extension.getURL("img/arrow_back.svg");
-    $("#ex-score #back-button").css("background-image",`url(${iconPass})`);
-    $("#ex-score #back-button").on("click", callback);
-}
-
-function removeBackButton(){
-    const iconPass =  chrome.extension.getURL("img/data_usage.svg");
-    $("#ex-score #back-button").css("background-image",`url(${iconPass})`);
-    $("#ex-score #back-button").off("click");
-}
-
 class App{
     constructor () {
         this.pages = [];
@@ -72,10 +11,16 @@ class App{
         const id = this.pages.length;
         this.pages[id] = new Page(id,obj);
         this.pages[id].render("#ex-score");
+        if(id > 0){
+            this.pages[id -1].hide();
+        }
     }
     removePage(id){
         this.pages[id].remove();
         this.pages.splice(id, 1);
+        if(id > 0){
+            this.pages[id -1].show();
+        }
     }
 }
 
@@ -86,38 +31,49 @@ class Page{
         this.contents.items.forEach(item => {
             item.pageId = this.id;
         });
+        this.element = null;
     }
     render(parentElement){
         const id = this.id;
         $(parentElement).append(`
         <section id="ex-page-${this.id}" class="ex-page">
         <header id="ex-header-${this.id}" class="ex-header">
-            <div id="back-button"></div><p>${this.contents.title}</p>
+            <div class="header-button"></div><p>${this.contents.title}</p>
         </header>
         <div id="ex-body-${this.id}" class="ex-body"></div>
         </section>` 
         );
-        $(`#ex-page-${this.id}`).css('z-index', id);
+        this.element = $(`#ex-page-${this.id}`).get();
+        $(this.element).css('z-index', id);
         if(this.id == 0){
             const iconPass =  chrome.extension.getURL("img/data_usage.svg");
-            $(`#ex-page-${this.id} #back-button`).css("background-image",`url(${iconPass})`);
+            $(this.element).find('.header-button').css("background-image",`url(${iconPass})`);
         }else{
             const iconPass =  chrome.extension.getURL("img/arrow_back.svg");
-            $(`#ex-page-${this.id} #back-button`).css("background-image",`url(${iconPass})`);
-            $(`#ex-page-${this.id} #back-button`).on("click", this.contents.callback);
+            $(this.element).find('.header-button').css("background-image",`url(${iconPass})`);
+            $(this.element).find('.header-button').attr('id', 'back-button');
+            $(this.element).find('.header-button').on("click", this.contents.callback);
         }
         this.contents.items.forEach(item => {
             item.render(`#ex-body-${this.id}`);
         });
     }
     remove(){
-        $(` #ex-page-${this.id}`).addClass("pageOut");
+        $(this.element).removeClass("pageShow"); 
+        $(this.element).addClass("pageOut");
         setTimeout(()=>{
             this.contents.items.forEach(item => {
                 item.remove();
             });
-            $(`#ex-page-${this.id}`).remove();
+            $(this.element).remove();
         },400);
+    }
+    show(){
+        $(this.element).removeClass('pageHide');
+        $(this.element).addClass('pageShow');
+    }
+    hide(){
+        $(this.element).addClass('pageHide');
     }
 }
 
@@ -133,6 +89,24 @@ class ProgressBar{
                  <div class="bar"><span class="bar-text">${this.val}%</span><div class="bar-val" style="width:${this.val}%;"><span class="bar-val-text">${this.val}%</span></div></div>
             </div>
         `);
+    }
+    remove(){
+
+    }
+}
+
+class TabControl{
+    constructor (obj) {
+        this.contents = obj;
+        this.pageId = null;
+        this.element = null;
+    }
+    render(parentElement){
+        for (let i = 0; i < this.contents.items.length; i++) {
+            const item = this.contents.items[i];
+            item.pageId = this.pageId;
+        }
+
     }
     remove(){
 
